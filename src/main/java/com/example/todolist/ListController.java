32295d035cea92116539731a9e8cb63ace63156f
kevinnovianp <kevinnovianprafajar@gmail.com>
2022-10-20 10:22:58 +0700

    controller

--- src/main/java/com/example/todolist/ListController.java
+++ src/main/java/com/example/todolist/ListController.java
@@ -1,5 +1,42 @@
 package com.example.todolist;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("")
 public class ListController {
-    
+    @Autowired
+    private ListService service;
+
+    @PostMapping("/add")
+    public String addList(@RequestBody CustomList list){
+        service.addList(list);
+        return "New list added!";
+    }
+
+    @GetMapping("/all")
+    public List<CustomList> getAllLists(){
+        return service.getAllLists();
+    }
+
+    @GetMapping("/find/{id}")
+    public Optional<CustomList> findList(@PathVariable int id){
+        return service.findList(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public String updateList(@RequestBody CustomList list, @PathVariable int id){
+        service.updateList(list, id);
+        return "Exist list updated!";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteList(@PathVariable int id){
+        service.deleteList(id);
+        return "Chosen list deleted!";
+    }
 }
